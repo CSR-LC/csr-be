b4be660afa53b307fca7098a57ce2a64c1aefab3
Daria Kuznetsova <dkuznertsova@infoblox.com>
2022-07-18 10:16:59 +0200

    added tests for repo with limit and offset

--- swagger/repositories/active_area_test.go
+++ swagger/repositories/active_area_test.go
@@ -34,6 +34,13 @@ func (s *ActiveAreasSuite) SetupTest() {
 	s.activeAreas = make(map[int]string)
 	s.activeAreas[1] = "area 1"
 	s.activeAreas[2] = "area 2"
+	s.activeAreas[3] = "area 3"
+	s.activeAreas[4] = "area 4"
+	s.activeAreas[5] = "area 5"
+	s.activeAreas[6] = "area 6"
+	s.activeAreas[7] = "area 7"
+	s.activeAreas[8] = "area 8"
+	s.activeAreas[9] = "area 9"
 
 	_, err := s.client.ActiveArea.Delete().Exec(s.ctx)
 	if err != nil {
@@ -63,6 +70,38 @@ func (s *ActiveAreasSuite) TestActiveAreaRepository_AllActiveAreas() {
 	assert.Equal(t, len(s.activeAreas), len(activeAreas))
 	for _, value := range activeAreas {
 		assert.Contains(t, s.activeAreas, value.ID)
+	}
+}
+
+func (s *ActiveAreasSuite) TestActiveAreaRepository_LimitActiveAreas() {
+	t := s.T()
+	limit := 3
+	offset := 0
+	repository := NewActiveAreaRepository(s.client)
+	activeAreas, err := repository.AllActiveAreas(s.ctx, limit, offset)
+	if err != nil {
+		t.Fatal(err)
+	}
+	assert.Equal(t, 3, len(activeAreas))
+	for i, value := range activeAreas {
+		assert.Contains(t, s.activeAreas, value.ID)
+		assert.Equal(t, s.activeAreas[i+1], value.Name)
+	}
+}
+
+func (s *ActiveAreasSuite) TestActiveAreaRepository_OffsetActiveAreas() {
+	t := s.T()
+	limit := 6
+	offset := 6
+	repository := NewActiveAreaRepository(s.client)
+	activeAreas, err := repository.AllActiveAreas(s.ctx, limit, offset)
+	if err != nil {
+		t.Fatal(err)
+	}
+	assert.Equal(t, 3, len(activeAreas))
+	for i, value := range activeAreas {
+		assert.Contains(t, s.activeAreas, value.ID)
+		assert.Equal(t, s.activeAreas[i+1+offset], value.Name)
 	}
 }
 
