70cd73e5ff06b67bd486b018d6d57ba5ded44132
Eugene Makarihkin <eugene@makarikhin.info>
2023-10-03 23:13:47 +0200

    Replaced filter from currentDate to ID

--- internal/repositories/order_status.go
+++ internal/repositories/order_status.go
@@ -139,7 +139,7 @@ func (r *orderStatusRepository) GetOrderCurrentStatus(ctx context.Context, order
 		WithOrder(func(query *ent.OrderQuery) {
 			query.WithUsers()
 		}).
-		Order(ent.Desc(orderstatus.FieldCurrentDate)).First(ctx)
+		Order(ent.Desc(orderstatus.FieldID)).First(ctx)
 	if err != nil {
 		return nil, fmt.Errorf("status history error, failed to get statuses: %s", err)
 	}
